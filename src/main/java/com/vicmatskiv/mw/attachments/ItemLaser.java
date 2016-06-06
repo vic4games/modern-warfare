d11cc4943b98907f014ff7197498bb523e694ab9
Victor Matskiv <victor.matskiv@yahoo.com>
2016-06-05 21:40:45 -0700

    Added laser

--- src/main/java/com/vicmatskiv/mw/attachments/ItemLaser.java
+++ src/main/java/com/vicmatskiv/mw/attachments/ItemLaser.java
@@ -4,6 +4,7 @@ import com.vicmatskiv.mw.ModernWarfareMod;
 import com.vicmatskiv.mw.models.Laser;
 import com.vicmatskiv.weaponlib.AttachmentCategory;
 import com.vicmatskiv.weaponlib.ItemAttachment;
+import com.vicmatskiv.weaponlib.LaserBeamRenderer;
 import com.vicmatskiv.weaponlib.Weapon;
 
 public class ItemLaser extends ItemAttachment<Weapon>
@@ -18,6 +19,8 @@ public class ItemLaser extends ItemAttachment<Weapon>
 				(attachment, weapon, player) -> {
 					weapon.changeRecoil(player, 1);
 				});
+		
+		setPostRenderer(new LaserBeamRenderer());
 		
 		setMaxStackSize(1);
 		setUnlocalizedName(ModernWarfareMod.MODID + "_" + "Laser"); 
