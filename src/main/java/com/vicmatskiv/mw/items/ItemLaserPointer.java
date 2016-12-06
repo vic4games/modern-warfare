57fa81eb8603b1ce99cab44d49398f171de6d66b
victor-jr <victor-jr@192.168.8.101>
2016-12-05 19:37:00 -0800

    Misc changes

---
+++ src/main/java/com/vicmatskiv/mw/items/ItemLaserPointer.java
@@ -0,0 +1,17 @@
+package com.vicmatskiv.mw.items;
+
+import net.minecraft.item.Item;
+
+import com.vicmatskiv.mw.ModernWarfareMod;
+
+public class ItemLaserPointer extends Item
+{
+	public ItemLaserPointer()
+	{
+		setMaxStackSize(64);
+		setUnlocalizedName(ModernWarfareMod.MODID + "_" + "LaserPointer"); 
+		setTextureName(ModernWarfareMod.MODID + ":" + "LaserPointer");
+		setCreativeTab(ModernWarfareMod.gunsTab);
+	}
+	
+}
---
+++ src/main/java/com/vicmatskiv/mw/resources/ItemMiniSteelPlate.java
@@ -0,0 +1,17 @@
+package com.vicmatskiv.mw.resources;
+
+import net.minecraft.item.Item;
+
+import com.vicmatskiv.mw.ModernWarfareMod;
+
+public class ItemMiniSteelPlate extends Item
+{
+	public ItemMiniSteelPlate()
+	{
+		setMaxStackSize(64);
+		setUnlocalizedName(ModernWarfareMod.MODID + "_" + "MiniSteelPlate"); 
+		setTextureName(ModernWarfareMod.MODID + ":" + "MiniSteelPlate");
+		setCreativeTab(ModernWarfareMod.gunsTab);
+	}
+	
+}
