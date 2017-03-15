e25b88315a9d40dc5d3b30055f19cfa1324e053f
Victor Matskiv <victor.matskiv@yahoo.com>
2017-03-15 08:42:52 -0700

    Version 1.8.1

--- src/main/java/com/vicmatskiv/mw/ModernWarfareMod.java
+++ src/main/java/com/vicmatskiv/mw/ModernWarfareMod.java
@@ -28,7 +28,7 @@ public class ModernWarfareMod {
     private static final String CONFIG_PROPERTY_ORE_GENERATION_ENABLED = "Ore generation enabled";
     private static final String MODERN_WARFARE_CONFIG_FILE_NAME = "ModernWarfare.cfg";
     public static final String MODID = "mw";
-    public static final String VERSION = "1.8";
+    public static final String VERSION = "1.8.1";
     
     @SidedProxy(serverSide = "com.vicmatskiv.weaponlib.CommonModContext", clientSide = "com.vicmatskiv.weaponlib.ClientModContext")
     public static ModContext MOD_CONTEXT;
