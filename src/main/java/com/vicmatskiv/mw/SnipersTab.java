9be1aef67839f31f0a9265752cbfd5d00a84ac69
Victor Matskiv <victor.matskiv@yahoo.com>
2017-04-05 22:10:30 -0700

    Incremental checkin. Mod runs

--- src/main/java/com/vicmatskiv/mw/SnipersTab.java
+++ src/main/java/com/vicmatskiv/mw/SnipersTab.java
@@ -2,6 +2,7 @@ package com.vicmatskiv.mw;
 
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
@@ -14,9 +15,9 @@ public class SnipersTab extends CreativeTabs
 
 	@Override
 	@SideOnly(Side.CLIENT)
-	public Item getTabIconItem()
+	public ItemStack getTabIconItem()
 	{
-		return CommonProxy.HecateII;
+		return new ItemStack(CommonProxy.HecateII);
 	}
 
 }
