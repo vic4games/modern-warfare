949fbda5c32e7e51d5e8df3af975c62e9d2c3fcd
Victor V. Matskiv <victor-jr@Victors-iMac.local>
2017-04-10 20:28:46 -0700

    Fixed block drops

--- src/main/java/com/vicmatskiv/mw/blocks/BlockRubyOre.java
+++ src/main/java/com/vicmatskiv/mw/blocks/BlockRubyOre.java
@@ -8,24 +8,26 @@ import com.vicmatskiv.mw.ModernWarfareMod;
 import net.minecraft.block.Block;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.item.Item;
 
 public class BlockRubyOre extends Block {
-	
-	public BlockRubyOre()
-	{
-		super(Material.ROCK);
-		//setRegistryName(ModernWarfareMod.MODID + "_" + "RubyOre");
-		setUnlocalizedName(ModernWarfareMod.MODID + "_" + "RubyOre");
-		setHardness(6F);
-		setResistance(600000F);
-		setSoundType(SoundType.STONE);
-		setHarvestLevel("pickaxe", 2);
-		setCreativeTab(ModernWarfareMod.gunsTab);
-	}
-	public Item getItemDropped(int meta, Random rand, int fortune)
-		{
-			return CommonProxy.Ruby;
-		}
-		
-	}
+
+    public BlockRubyOre()
+    {
+        super(Material.ROCK);
+        //setRegistryName(ModernWarfareMod.MODID + "_" + "RubyOre");
+        setUnlocalizedName(ModernWarfareMod.MODID + "_" + "RubyOre");
+        setHardness(6F);
+        setResistance(600000F);
+        setSoundType(SoundType.STONE);
+        setHarvestLevel("pickaxe", 2);
+        setCreativeTab(ModernWarfareMod.gunsTab);
+    }
+
+    @Override
+    public Item getItemDropped(IBlockState state, Random rand, int fortune) {
+        return CommonProxy.Ruby;
+    }
+
+}
