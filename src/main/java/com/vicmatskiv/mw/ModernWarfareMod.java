61bca55f8b243da1cd904867cce12615b0c24798
Victor V. Matskiv <victor-jr@Victors-iMac.local>
2017-04-17 17:41:35 -0700

    1.9.1

--- src/main/java/com/vicmatskiv/mw/ModernWarfareMod.java
+++ src/main/java/com/vicmatskiv/mw/ModernWarfareMod.java
@@ -23,7 +23,7 @@ public class ModernWarfareMod {
 	private static final String CONFIG_PROPERTY_ORE_GENERATION_ENABLED = "Ore generation enabled";
 	private static final String MODERN_WARFARE_CONFIG_FILE_NAME = "ModernWarfare.cfg";
 	public static final String MODID = "mw";
-	public static final String VERSION = "1.9";
+	public static final String VERSION = "1.9.1";
 
 	@SidedProxy(serverSide = "com.vicmatskiv.weaponlib.CommonModContext", clientSide = "com.vicmatskiv.weaponlib.ClientModContext")
 	public static ModContext MOD_CONTEXT;
@@ -84,6 +84,6 @@ public class ModernWarfareMod {
 	// ItemRecipes
 	@EventHandler
 	public void init(FMLInitializationEvent event) {
-
+	    RecipeManager.init();
 	}
 }
