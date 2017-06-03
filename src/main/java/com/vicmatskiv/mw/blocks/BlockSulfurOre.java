2bc038fd4765811e78d924d843ac8643a8df7947
Victor V. Matskiv <victor-jr@Victors-iMac.local>
2017-06-02 23:30:28 -0700

    Merge

--- src/main/java/com/vicmatskiv/mw/blocks/BlockSulfurOre.java
+++ src/main/java/com/vicmatskiv/mw/blocks/BlockSulfurOre.java
@@ -8,26 +8,27 @@ import com.vicmatskiv.mw.Ores;
 import net.minecraft.block.Block;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.item.Item;
 
-public class BlockSulfurOre extends Block 
+public class BlockSulfurOre extends Block
 
 {
-	
-	public BlockSulfurOre()
-	{
-		super(Material.ROCK);
-		//setRegistryName(ModernWarfareMod.MODID + "_" + "SulfurOre");
-		setUnlocalizedName(ModernWarfareMod.MODID + "_" + "SulfurOre");
-		setHardness(4F);
-		setResistance(600000F);
-		setSoundType(SoundType.STONE);
-		setHarvestLevel("pickaxe", 2);
-		setCreativeTab(ModernWarfareMod.gunsTab);
-	}
-	public Item getItemDropped(int meta, Random rand, int fortune)
-		{
-			return Ores.SulfurDust;
-		}
-		
-	}
+
+    public BlockSulfurOre() {
+        super(Material.ROCK);
+        // setRegistryName(ModernWarfareMod.MODID + "_" + "SulfurOre");
+        setUnlocalizedName(ModernWarfareMod.MODID + "_" + "SulfurOre");
+        setHardness(4F);
+        setResistance(600000F);
+        setSoundType(SoundType.STONE);
+        setHarvestLevel("pickaxe", 2);
+        setCreativeTab(ModernWarfareMod.gunsTab);
+    }
+
+    @Override
+    public Item getItemDropped(IBlockState state, Random rand, int fortune) {
+        return Ores.SulfurDust;
+    }
+
+}
