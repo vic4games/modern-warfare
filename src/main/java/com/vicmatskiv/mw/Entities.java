481bf50d900cd5e4d3414a693f6dc5fe3a1126a1
Victor Matskiv <victor.matskiv@yahoo.com>
2017-08-25 20:03:12 -0700

    AI task compatibility

--- src/main/java/com/vicmatskiv/mw/Entities.java
+++ src/main/java/com/vicmatskiv/mw/Entities.java
@@ -1,5 +1,7 @@
 package com.vicmatskiv.mw;
 
+import static com.vicmatskiv.weaponlib.compatibility.CompatibilityProvider.compatibility;
+
 import com.vicmatskiv.weaponlib.Weapon;
 import com.vicmatskiv.weaponlib.ai.EntityAIAttackRangedWeapon;
 import com.vicmatskiv.weaponlib.ai.EntityConfiguration;
@@ -8,10 +10,8 @@ import com.vicmatskiv.weaponlib.compatibility.CompatibleBiomeType;
 import com.vicmatskiv.weaponlib.grenade.ItemGrenade;
 
 import net.minecraft.entity.EntityCreature;
-import net.minecraft.entity.ai.EntityAIAvoidEntity;
 import net.minecraft.entity.ai.EntityAIHurtByTarget;
 import net.minecraft.entity.ai.EntityAILookIdle;
-import net.minecraft.entity.ai.EntityAINearestAttackableTarget;
 import net.minecraft.entity.ai.EntityAISwimming;
 import net.minecraft.entity.ai.EntityAIWander;
 import net.minecraft.entity.ai.EntityAIWatchClosest;
@@ -19,8 +19,6 @@ import net.minecraft.entity.monster.EntityIronGolem;
 import net.minecraft.entity.passive.EntityWolf;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.world.EnumDifficulty;
-import net.minecraftforge.common.BiomeDictionary;
-import net.minecraftforge.common.BiomeDictionary.Type;
 
 public class Entities {
 
@@ -42,14 +40,14 @@ public class Entities {
             .withTexturedModelVariant("com.vicmatskiv.weaponlib.compatibility.CompatibleModelCustomMob", "skeleton.png")
             .withTexturedModelVariant("com.vicmatskiv.weaponlib.compatibility.CompatibleModelCustomMob", "skeleton.png")
             .withAiTask(1, e -> new EntityAISwimming(e))
-            .withAiTask(3, e -> new EntityAIAvoidEntity<>((EntityCreature)e, EntityWolf.class, 6.0F, 1.0D, 1.2D))
+            .withAiTask(3, e -> compatibility.createAiAvoidEntity(e, EntityWolf.class, 6.0F, 1.0D, 1.2D))
             .withAiTask(5, e -> new EntityAIAttackRangedWeapon((EntityCustomMob)e, 1.0D, 10, 20.0f, 0.04f, Weapon.class, ItemGrenade.class))
             .withAiTask(6, e -> new EntityAIWander((EntityCreature)e, 1.0D))
             .withAiTask(7, e -> new EntityAIWatchClosest(e, EntityPlayer.class, 20.0F))
             .withAiTask(7, e -> new EntityAILookIdle(e))
-            .withAiTargetTask(1, e -> new EntityAIHurtByTarget((EntityCreature)e, false, new Class[0]))
-            .withAiTargetTask(2, e -> new EntityAINearestAttackableTarget<>((EntityCreature)e, EntityPlayer.class, true))
-            .withAiTargetTask(3, e -> new EntityAINearestAttackableTarget<>((EntityCreature)e, EntityIronGolem.class, true))
+            .withAiTargetTask(1, e -> new EntityAIHurtByTarget((EntityCreature)e, false))
+            .withAiTargetTask(2, e -> compatibility.createAINearestAttackableTarget(e, EntityPlayer.class, true))
+            .withAiTargetTask(3, e -> compatibility.createAINearestAttackableTarget(e, EntityIronGolem.class, true))
             .register(ModernWarfareMod.MOD_CONTEXT);
     }
 
