aaa727d99148d855380ce46b348bc0ac49a4c6d1
Victor Matskiv <victor.matskiv@yahoo.com>
2018-04-29 12:16:06 -0700

    Added support for proning aiming

--- src/main/java/com/vicmatskiv/mw/PlayerAnimations.java
+++ src/main/java/com/vicmatskiv/mw/PlayerAnimations.java
@@ -85,6 +85,45 @@ public class PlayerAnimations {
                             GL11.glRotatef(-2.0F, 0.0F, 1.0F, 1.0F);
                             GL11.glScalef(1f, 1f, 1f);
                         }))
+                
+                .withProningAimingTransition(new MultipartTransition<Part, RenderContext<RenderableState>>(
+                        Part.MAIN, renderContext -> {
+                            GL11.glTranslatef(0f, 1.36f, -0.5f);
+                            GL11.glRotatef(90.0F, 1.0F, 0.0F, 0F);
+                            GL11.glRotatef(-5.0F, 0.0F, 1.0F, 0F);
+
+                        }, 200, 0)
+                        .withPartPositionFunction(Part.HEAD, rc -> {
+                            GL11.glTranslatef(0f, 0f, -0.08f);
+//                            GL11.glRotatef(-10.0F, 0.0F, 1.0F, 1.0F);
+                            GL11.glRotatef(-90.0F, 1.0F, 0.0F, 0.0F);
+                            GL11.glScalef(1f, 1f, 1f);
+                        })
+                        .withPartPositionFunction(Part.LEFT_HAND, rc -> {
+                            GL11.glTranslatef(-0.1f, 0.1f, 0f);
+                            GL11.glRotatef(-180.0F, 1.0F, 0.0F, 0.0F);
+                            GL11.glRotatef(10.0F, 0.0F, 0.0F, 1.0F);
+                            GL11.glScalef(1f, 1f, 1f);
+                        })
+                        .withPartPositionFunction(Part.RIGHT_HAND, rc -> {
+                            //System.out.println("Render aiming state");
+                            GL11.glTranslatef(0.08f, 0.1f, 0f);
+                            GL11.glRotatef(-160.0F, 1.0F, 0.0F, 0.0F);
+                            GL11.glRotatef(-5.0F, 0.0F, 0.0F, 1.0F);
+                            //GL11.glRotatef(-30.0F, 0.0F, 1.0F, 0.0F);
+                            GL11.glScalef(1f, 1f, 1f);
+                        })
+                        .withPartPositionFunction(Part.LEFT_LEG, rc -> {
+                            GL11.glTranslatef(0.04f, 0f, 0f);
+                            GL11.glRotatef(5.0F, 0.0F, 1.0F, 1.0F);
+                            GL11.glScalef(1f, 1f, 1f);
+                        })
+                        .withPartPositionFunction(Part.RIGHT_LEG, rc -> {
+                            GL11.glTranslatef(0.05f, 0.4f, -0.02f);
+                            GL11.glRotatef(25.0F, 0.0F, 1.0F, 1.0F);
+                            GL11.glScalef(1f, 0.5f, 1f);
+                        }))
+
                 .build();
         
         ModernWarfareMod.MOD_CONTEXT.setPlayerTransitionProvider(playerTransitionProvider);
